781befa671fc98bbc89deab6fb6b7733f7100ded
MalikNegmatov <maliknegmatov@gmail.com>
2023-07-07 09:46:53 +0300

    5. Some code review

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class Main {
 
+    public static final List<Integer> ints = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    public static final List<String> words = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+
     public static void main(String[] args) {
         System.out.println("Задача 1");
         task1();
@@ -14,10 +17,8 @@ public class Main {
     }
 
     public static void task1(){
-        List<Integer> intList = new ArrayList<>(List.of(1, 1, 2, 2, 3, 4, 4, 5, 5, 6, 7));
-        String s = ", ";
-        StringJoiner j = new StringJoiner(s);
-        for (int i : intList) {
+        StringJoiner j = new StringJoiner(", ");
+        for (int i : ints) {
             if (i % 2 != 0) {
                 j.add(String.valueOf(i));
             }
@@ -27,9 +28,8 @@ public class Main {
     }
 
     public static void task2(){
-        Set<Integer> setOfInts = new HashSet<>(List.of(1, 1, 2, 3, 4, 10, 4, 5, 5, 6, 6, 6, 7));
-        String s = ", ";
-        StringJoiner j = new StringJoiner(s);
+        StringJoiner j = new StringJoiner(", ");
+        Set<Integer> setOfInts = new HashSet<>(ints);
         for (int i : setOfInts) {
             if (i % 2 == 0) {
                 j.add(String.valueOf(i));
@@ -39,18 +39,12 @@ public class Main {
         System.out.println();
     }
     public static void task3(){
-        String s = ", ";
-        StringJoiner j = new StringJoiner(s);
-        List<String> w = new ArrayList<>(List.of("стол", "стул", "парта", "стул", "сто", "ступа", "стол", "ступа", "сто"));
-        Set<String> setOfWords = new HashSet<>(w);
-        for (String x : setOfWords) {
-            j.add(x);
-        }
-        System.out.println(j);
+        Set<String> setOfWords = new HashSet<>(words);
+        System.out.println(setOfWords);
         System.out.println();
     }
     public static void task4(){
-        List<String> s = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+        List<String> s = new ArrayList<>(words);
         Set<String> d = new HashSet<>(s);
         for (String x : d) {
             System.out.println(Collections.frequency(s, x));
