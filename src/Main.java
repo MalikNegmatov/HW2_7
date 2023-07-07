86cc83930726dab5c9b6d5b3c967740a18c72395
MalikNegmatov <maliknegmatov@gmail.com>
2023-07-07 09:21:00 +0300

    1. Напишите код, с помощью которого можно напечатать только нечетные числа в консоль.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,8 +13,17 @@ public class Main {
     }
 
     public static void task1(){
-
+        List<Integer> intList = new ArrayList<>(List.of(1, 1, 2, 2, 3, 4, 4, 5, 5, 6, 7));
+        String s = ", ";
+        StringJoiner j = new StringJoiner(s);
+        for (int i : intList) {
+            if (i % 2 != 0) {
+                j.add(String.valueOf(i));
+            }
+        }
+        System.out.println(j);
     }
+
     public static void task2(){
 
     }
