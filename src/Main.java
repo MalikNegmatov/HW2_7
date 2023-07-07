a5ece11eb8611764a5fd32e5931d2bab68ea9380
MalikNegmatov <maliknegmatov@gmail.com>
2023-07-07 09:25:42 +0300

    2. Напишите код, с помощью которого можно напечатать только четные числа без повторений в порядке возрастания.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringJoiner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Задача 1");
         task1();
+        System.out.println("Задача 2");
         task2();
         task3();
         task4();
@@ -22,10 +21,20 @@ public class Main {
             }
         }
         System.out.println(j);
+        System.out.println();
     }
 
     public static void task2(){
-
+        Set<Integer> setOfInts = new HashSet<>(List.of(1, 1, 2, 3, 4, 10, 4, 5, 5, 6, 6, 6, 7));
+        String s = ", ";
+        StringJoiner j = new StringJoiner(s);
+        for (int i : setOfInts) {
+            if (i % 2 == 0) {
+                j.add(String.valueOf(i));
+            }
+        }
+        System.out.println(j);
+        System.out.println();
     }
     public static void task3(){
 
